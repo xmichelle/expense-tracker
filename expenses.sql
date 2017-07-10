e8d0a6e516e8f0824f04a70b782c97219dd68b0b
xu.miche@gmail.com <xu.miche@gmail.com>
2017-07-10 15:59:16 -0700

    Created an expenditures table. #1

---
+++ expenses.sql
@@ -0,0 +1,9 @@
+CREATE TABLE expenditures (
+  id                 serial,
+  category_id        integer,
+  item               text,
+  transaction_date   date,
+  cost_amount        numeric(1000, 2)
+);
+
+/*SELECT TO_CHAR(date, 'Mon dd, yyyy');*/
