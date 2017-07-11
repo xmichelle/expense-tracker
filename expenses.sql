3da857b5909499cae083665e87e3b3f8bfb5e5d2
xu.miche@gmail.com <xu.miche@gmail.com>
2017-07-11 10:49:03 -0700

    Created an app.use method for the static middleware. #1

--- expenses.sql
+++ expenses.sql
@@ -7,3 +7,5 @@ CREATE TABLE expenditures (
 );
 
 /*SELECT TO_CHAR(date, 'Mon dd, yyyy');*/
+
+SELECT * FROM expenditures
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const path = require('path')
 
 const expenditures = []
+
+const publicPath = path.join(__dirname, 'public')
+const staticMiddleware = express.static(publicPath)
+app.use(staticMiddleware)
 
 app.get('/expenditures', (req, res) => {
   res.json(expenditures)
 })
 
 app.use(bodyParser.json())
+
 
 app.post('/expenditures', (req, res) => {
   expenditures.push(req.body)
