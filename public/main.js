63e2682ca3f658680a3bfae1f8166798ddfaa8a9
xu.miche@gmail.com <xu.miche@gmail.com>
2017-07-13 17:04:32 -0700

    Created render functions for the expense table. #2

--- public/main.js
+++ public/main.js
@@ -1,19 +1,62 @@
 
 const $expenseForm = document.querySelector('#expense-form')
-const $expenseTable = document.querySelector('#expense-table')
+const $expenseTable = document.querySelector('.expense-table')
+const $expenseHeader = document.querySelector('.expense-head')
+const $expenseBody = document.querySelector('.expense-body')
 
-function renderExpenseData(data) {
-  return
+function renderExpenseHeaders(header) {
+  const $container = document.createElement('tr')
+  const $categoryHead = document.createElement('th')
+  const $expenseHead = document.createElement('th')
+  const $dateHead = document.createElement('th')
+  const $amountHead = document.createElement('th')
+
+  $categoryHead.setAttribute('scope', 'col')
+  $expenseHead.setAttribute('scope', 'col')
+  $dateHead.setAttribute('scope', 'col')
+  $amountHead.setAttribute('scope', 'col')
+
+  $categoryHead.textContent = 'Category'
+  $expenseHead.textContent = 'Expenses'
+  $dateHead.textContent = 'Transaction Date'
+  $amountHead.textContent = 'Amount'
+
+  $container.appendChild($categoryHead)
+  $container.appendChild($expenseHead)
+  $container.appendChild($dateHead)
+  $container.appendChild($amountHead)
+
+  return $container
 }
 
-fetch('/expenditures')
-  .then(res => res.json()) // parse to JS object
-  .then(jsonData => {
-    console.log(jsonData)
-  })
-  .catch(err => {
-    console.log(err)
-  })
+function renderExpenseData(data) {
+  const $container = document.createElement('tr')
+  const $categoryData = document.createElement('td')
+  const $expenseData = document.createElement('td')
+  const $dateData = document.createElement('td')
+  const $amountData = document.createElement('td')
+
+  $categoryData.textContent = data.category_id
+  $expenseData.textContent = data.item
+  $dateData.textContent = data.transaction_date
+  $amountData.textContent = data.amount
+
+  $container.appendChild($categoryData)
+  $container.appendChild($expenseData)
+  $container.appendChild($dateData)
+  $container.appendChild($amountData)
+
+  return $container
+}
+
+// fetch('/expenditures')
+//   .then(res => res.json()) // parse to JS object
+//   .then(jsonData => {
+//     console.log(jsonData)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
 
 
 
